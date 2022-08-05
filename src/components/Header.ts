a584ce48f6c247eafd0ee9c0bb0fa6de777c2b9d
Travis <travis.m.bruce@gmail.com>
2022-08-04 22:34:05 -0700

    Finalize header layout

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,7 +1,10 @@
 import Menu from '@assets/menu.svg';
 import Home from '@assets/home.svg';
 import Search from '@assets/search.svg';
-
+import Plus from '@assets/plus.svg';
+import Question from '@assets/question.svg';
+import Bell from '@assets/bell.svg';
+import Avatar from '@assets/man.png';
 import iconBtn from "../utils/iconBtn";
 import htmlElement from "../utils/htmlElement";
 
@@ -13,8 +16,18 @@ const Header = () => {
     let inputField = htmlElement('input', 'header-search') as HTMLInputElement;
     inputField.placeholder = 'Find';
     inputField.append(searchIcon);
-
     headerLeft.append(menuIcon, homeIcon, inputField);
+
+    let headerRight = document.querySelector('.header-right')!;
+    let addIcon = iconBtn(Plus, 'plus-icon icon-btn');
+    let circleIcon = htmlElement('div', 'circle-icon icon-btn');
+    let tasksDone = htmlElement('div', 'tasks-done');
+    tasksDone.textContent = '3/4';
+    let helpIcon = iconBtn(Question, 'question-icon icon-btn');
+    let notificationIcon = iconBtn(Bell, 'bell-icon icon-btn');
+    let gearIcon = iconBtn(Avatar, 'avatar-icon icon-btn');
+
+    headerRight.append(addIcon, circleIcon, tasksDone, helpIcon, notificationIcon, gearIcon);
 }
 
 export default Header;
