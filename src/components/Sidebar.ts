687f27de44f4f0475b7794214c4e7266626d8816
Travis <travis.m.bruce@gmail.com>
2022-08-06 21:44:18 -0700

    Update styling to more closely match target site

--- src/components/Sidebar.ts
+++ src/components/Sidebar.ts
@@ -1,13 +1,24 @@
+import SidebarItem from "./SidebarItem";
 import Inbox from '@assets/inbox.svg';
-
+import CalendarDay from '@assets/calendar-blank-outline.svg';
+import CalendarMonth from '@assets/calendar-month-outline.svg';
+import Squares from '@assets/squares.svg';
 
 import events from '../store/events';
 
 const Sidebar = () => {
     let sidebarOpen = true;
     let nav = document.querySelector('.nav')! as HTMLDivElement;
+    let inbox = new SidebarItem('inbox', Inbox, 'Inbox', '8');
+    let today = new SidebarItem('today', CalendarDay, 'Today', '4');
+    let month = new SidebarItem('upcoming', CalendarMonth, 'Upcoming', '2');
+    let filters = new SidebarItem('filters', Squares, 'Filters & Labels');
 
+    nav.append(inbox.render(), today.render(), month.render(), filters.render());
 
+    const toggleActive = () => {
+
+    }
 
     const toggleSidebar = () => {
         if (sidebarOpen) {
@@ -18,7 +29,6 @@ const Sidebar = () => {
             sidebarOpen = !sidebarOpen;
         }
     }
-
     events.subscribe('sidebarToggle', toggleSidebar)
 }
 
--- src/components/SidebarItem.ts
+++ src/components/SidebarItem.ts
@@ -32,3 +32,5 @@ class SidebarItem {
     }
 
 }
+
+export default SidebarItem;
