11dae2113374f0a02c4bebbb698c0f176e0dc043
Travis <travis.m.bruce@gmail.com>
2022-08-09 10:16:16 -0700

    Color correction

--- src/store/Todos.ts
+++ src/store/Todos.ts
@@ -1,4 +1,24 @@
 class Todo {
     title: string;
+    id: string;
+    completed: boolean;
+    dueDate: Date;
+    children: string[];
+    labels: string[];
+    priority: number;
+    project: string;
+
+    constructor(title: string, id:string, completed: boolean, dueDate: Date, children: string[], labels: string[], priority: number, project: string) {
+        this.title = title;
+        this.id = id;
+        this.completed = completed;
+        this.dueDate = dueDate;
+        this.children = children;
+        this.labels = labels;
+        this.priority = priority;
+        this.project = project;
+    }
+
+    
 
 }
--- src/utils/idGenerator.ts
+++ src/utils/idGenerator.ts
@@ -1,10 +1,11 @@
-const getId = (length: number) => {
+const getId = (length: number): string => {
     let result = '';
     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let charLength = characters.length;
     for ( let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charLength))
     }
+    return result;
 }
 
 export default getId;
