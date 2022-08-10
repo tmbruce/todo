069572dee2fd30d2a78be55aae2f610cd67a0b63
Travis <travis.m.bruce@gmail.com>
2022-08-10 09:23:28 -0700

    Add methods to projects store

--- src/store/projectStore.ts
+++ src/store/projectStore.ts
@@ -18,8 +18,14 @@ const projectStore = (() => {
     }
 
     const updateProject = (project: Project) => {
-
+        const index = projects.findIndex(p => p.id === project.id);
+        projects[index] = project;
+        update();
     }
+
+    events.subscribe('newProject', (project: Project) => addProject(project));
+    events.subscribe('removeProject', (id: string) => removeProject(id));
+    events.subscribe('updateProject', (project: Project) => updateProject(project));
 
     return { addProject, removeProject, updateProject };
 
