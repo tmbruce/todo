773136ba39543c90066f7c4fa532502e51bdac9b
Travis <travis.m.bruce@gmail.com>
2022-08-09 19:06:08 -0700

    Projects tab sidebar styling, add projects store

--- src/store/projectStore.ts
+++ src/store/projectStore.ts
@@ -1,6 +1,27 @@
 import Project from "../models/Projects";
 import events from '../store/events';
 
-const projectStore = {
-    
-}
+const projectStore = (() => {
+
+    let projects: Project[] = [];
+
+    const update = () => events.update('projectStore', projects);
+
+    const addProject = (project: Project) => {
+        projects.push(project);
+        update();
+    }
+
+    const removeProject = (id: string) => {
+        projects = projects.filter(project => project.id !== id);
+        update();
+    }
+
+    const updateProject = (project: Project) => {
+
+    }
+
+    return { addProject, removeProject, updateProject };
+
+})();
+
