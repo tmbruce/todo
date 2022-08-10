836c553c6297138cdd4af1c6189bf5fd9ff775e0
Travis <travis.m.bruce@gmail.com>
2022-08-10 14:44:21 -0700

    Styling, project store methods

--- src/components/Sidebar.ts
+++ src/components/Sidebar.ts
@@ -4,6 +4,7 @@ import CalendarDay from '@assets/calendar-blank-outline.svg';
 import CalendarMonth from '@assets/calendar-month-outline.svg';
 import Squares from '@assets/squares.svg';
 import Add from '@assets/plus_dark.svg';
+import Caret from '@assets/caret-down.svg';
 
 import events from '../store/events';
 import htmlElement from "../utils/htmlElement";
@@ -12,17 +13,19 @@ import iconBtn from "../utils/iconBtn";
 const Sidebar = () => {
     const SIDEBAR_WIDTH = 19.5;
     let sidebarOpen = true;
+    let projectsOpen = true;
 
     let nav = document.querySelector('.nav')! as HTMLDivElement;
     let inbox = new SidebarItem('inbox', Inbox, 'Inbox', '8');
     let today = new SidebarItem('today', CalendarDay, 'Today', '4');
     let month = new SidebarItem('upcoming', CalendarMonth, 'Upcoming', '2');
     let filters = new SidebarItem('filters', Squares, 'Filters & Labels');
+    let caret = iconBtn(Caret, 'project-dropdown');
     let projects = htmlElement('h3', 'projects-header');
     projects.textContent = 'Projects';
     let projectAdd = iconBtn(Add, 'project-add icon-btn');
     let projectsContainer = htmlElement('div', 'projects-header-container');
-    projectsContainer.append(projects, projectAdd);
+    projectsContainer.append(caret, projects, projectAdd);
 
     nav.append(inbox.render(), today.render(), month.render(), filters.render(), projectsContainer);
 
@@ -31,6 +34,12 @@ const Sidebar = () => {
     }
 
     const addProject = () => events.update('addProject', {});
+
+    const toggleProjects = () => {
+        let caret = document.querySelector('.project-dropdown')! as HTMLImageElement;
+        caret.style.transform = projectsOpen ? 'rotate(-90deg)' : 'rotate(0deg)';
+        projectsOpen = !projectsOpen;
+    }
 
     const toggleSidebar = () => {
         let container = document.querySelector('.container') as HTMLDivElement;
@@ -44,7 +53,7 @@ const Sidebar = () => {
             sidebarOpen = !sidebarOpen;
         }
     }
-
+    document.querySelector('.projects-header-container')?.addEventListener('click', toggleProjects);
     document.querySelector('.project-add')?.addEventListener('click', addProject );  
 
     events.subscribe('sidebarToggle', toggleSidebar)
